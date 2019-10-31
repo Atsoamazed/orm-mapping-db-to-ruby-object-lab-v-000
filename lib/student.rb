3c5e13c1946c2c882fbf715bb6b2f93be0f4c423
Aderianna Williams <aderiannaw@yahoo.com>
2019-10-31 03:34:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,9 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
     student.grade = row[2]
    student
   end
